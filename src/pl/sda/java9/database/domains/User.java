fca5f90020f615b553082c01df00ff94b640353a
Jakub  Dobrogowski <dobrogowskijakub@gmail.com>
2018-10-05 00:17:41 +0200

    user domain

---
+++ src/pl/sda/java9/database/domains/User.java
@@ -0,0 +1,62 @@
+package pl.sda.java9.database.domains;
+
+public class User {
+
+
+    private Integer id;
+
+    private String name;
+    private String surname;
+    private String password;
+    private String login;
+
+    private Integer isAdmin;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public Integer getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Integer isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
